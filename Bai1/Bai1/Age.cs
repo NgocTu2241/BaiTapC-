69cf9d180632e8b2ce7e51429a174dcb4a3fb9fb
NgocTu2241 <80658387+NgocTu2241@users.noreply.github.com>
2022-04-22 22:14:40 +0700

    Add files via upload

---
+++ Bai1/Bai1/Age.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bai1
+{
+    internal class Age
+    {
+        int age;
+        static void Main(string[] args)
+        {
+            int a, b, c;
+            a = int.Parse(Console.ReadLine());
+            Ktra(a);
+            b = int.Parse(Console.ReadLine());
+            Ktra(b);
+            c = int.Parse(Console.ReadLine());
+            Ktra(c);
+            Console.ReadKey();
+        }
+        static void Ktra(int age)
+        {
+            if (age >= 60)
+                Console.WriteLine("Gia");
+            else if (age < 18)
+                Console.WriteLine("Tre em");
+            else
+                Console.WriteLine("Nguoi lon");
+        }
+    }
+}
